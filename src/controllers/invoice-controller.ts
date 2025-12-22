5dcc0cbac7b3e3d46c48b9d8e79933bc266307c2
az01r <paoloantonio.grosso@gmail.com>
2025-12-22 15:39:54 +0100

    Fixed amount number cast in invoice-controller.createInvoice

--- src/controllers/invoice-controller.ts
+++ src/controllers/invoice-controller.ts
@@ -7,7 +7,7 @@ class InvoiceController {
     const { taxProfileId, amount, status, currency } = req.body;
     const invoice = await InvoiceService.createInvoice(req.userId!, {
       taxProfileId,
-      amount,
+      amount: Number(amount),
       status,
       currency
     });
@@ -39,25 +39,15 @@ class InvoiceController {
   updateInvoice = async (req: Request, res: Response, _next: NextFunction) => {
     const { amount, status, currency } = req.body;
     const invoice = await InvoiceService.updateInvoice(req.userId!, req.params.id, {
-      amount,
+      amount: Number(amount),
       status,
       currency
     });
-    // implicit check in InvoiceService.updateInvoice
-    // if (!invoice) {
-    //   res.status(404);
-    //   throw new Error('Invoice not found.');
-    // }
     res.status(200).json({ invoice });
   }
 
   deleteInvoice = async (req: Request, res: Response, _next: NextFunction) => {
     await InvoiceService.deleteInvoice(req.userId!, req.params.id);
-    // implicit check in InvoiceService.deleteInvoice
-    // if (!invoice) {
-    //   res.status(404);
-    //   throw new Error('Invoice not found.');
-    // }
     res.status(200).json({ message: 'Invoice deleted.' });
   }
 }
