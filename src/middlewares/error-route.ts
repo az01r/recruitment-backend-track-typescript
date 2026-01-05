15ceb5f14ca661eb7f001325a0ff4faeec7987f1
az01r <paoloantonio.grosso@gmail.com>
2026-01-05 09:33:38 +0100

    Refactor error handling: centralized response status code in error-route, modified tests accordingly

--- src/middlewares/error-route.ts
+++ src/middlewares/error-route.ts
@@ -1,38 +1,6 @@
 import type { Request, Response, NextFunction } from 'express';
 import { ConflictError, ReqValidationError, ResourceNotFoundError, UnauthorizedError } from '../types/error.js';
 import logger from '../utils/logger.js';
-import { VALIDATION_ERROR } from '../utils/constants.js';
-
-// export default (error: Error, req: Request, res: Response, _next: NextFunction) => {
-//   res.statusCode < 400 && res.status(500);
-//   const message = error.message || "Internal Server Error";
-
-//   let validationErrors;
-//   if (error instanceof ReqValidationError) {
-//     validationErrors = error.errors
-//     error.statusCode && res.status(error.statusCode);
-
-//     logger.warn({
-//       err: error,
-//       validationErrors,
-//       path: req.path,
-//       method: req.method
-//     }, VALIDATION_ERROR);
-//   } else {
-//     logger.error({
-//       err: error,
-//       path: req.path,
-//       method: req.method,
-//       body: req.body
-//     }, 'Server error');
-//   }
-
-//   res.json({
-//     status: 'error',
-//     message,
-//     ...(validationErrors && { errors: validationErrors })
-//   });
-// };
 
 export default (error: Error, req: Request, res: Response, _next: NextFunction) => {
   const message = error.message || "Internal Server Error";
