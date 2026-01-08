1874cf66bb643c347a2f00cc4133a0a672cf263f
az01r <paoloantonio.grosso@gmail.com>
2026-01-08 18:01:28 +0100

    Removed unused response codes from is-auth middleware

--- src/middlewares/is-auth.ts
+++ src/middlewares/is-auth.ts
@@ -3,17 +3,15 @@ import jwt from "jsonwebtoken";
 import { UNAUTHORIZED } from "../utils/constants.js";
 import { UnauthorizedError } from "../types/error.js";
 
-export const isAuth = (req: Request, res: Response, next: NextFunction) => {
+export const isAuth = (req: Request, _res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
-    res.status(401);
     throw new UnauthorizedError(UNAUTHORIZED);
   }
   try {
     const decodedToken = jwt.verify(token!, process.env.JWT_SECRET!) as jwt.JwtPayload;
     req.userId = decodedToken.userId as string;
   } catch (e) {
-    res.status(401);
     throw new UnauthorizedError(UNAUTHORIZED);
   }
   next();
